79e2e477827713e3a295bd6b6a738070f0d4a852
JCHU634 <87676426+jchu634@users.noreply.github.com>
2023-04-19 00:10:24 +1200

    Renamed Ackerman as Ackermann

--- buildAckermanCanInterfaces.sh
+++
@@ -1,2 +0,0 @@
-source /opt/ros/humble/setup.bash
-colcon build --packages-select ackerman_can_interfaces
---
+++ buildAckermannCanInterfaces.sh
@@ -0,0 +1,2 @@
+source /opt/ros/humble/setup.bash
+colcon build --packages-select ackermann_can_interfaces
